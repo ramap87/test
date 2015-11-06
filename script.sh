9da53aa3e7bef547bfa676d3b6bf494ade01fa61
unknown <r.pallapotu@PDC1-87FCVQ1.dir.svc.accenture.com>
2015-11-06 17:35:15 +0530

    push

--- script.sh
+++ script.sh
@@ -16,7 +16,7 @@ for j in `ls $file1`; do
                     value=`sed -n '/<fields/,/<\/fields/p' "$file1/$j"`
 					#echo $value >> newobj.object
 					echo "welcome"
-					echo ' '$location'i \'$value'\ ' "$file2/$j"
+					echo ' '$location'i \$value\ ' "$file2/$j"
                                 #sed -i ''$location'i ${value}' "$file2/$j"
                                sed -i ''$location'i \'$value'\' "$file2/$j"
                                echo $j
