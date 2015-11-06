ff7662eb3bf03f74aba5fcce193b1de3938bfe62
unknown <r.pallapotu@PDC1-87FCVQ1.dir.svc.accenture.com>
2015-11-06 13:27:30 +0530

    hell

--- script.sh
+++ script.sh
@@ -10,7 +10,7 @@ for j in `ls $file1`; do
                                 location=$(grep -nr "<fields>" "$file2/$j" | head -1 | cut -d : -f 1)
                                 echo $location
                                 #echo "line number  of file2 $location"
-                                sed -ie '/<fields>/, /<\/fields>/ d ' "$file2/$j"
+                                sed -i '/<fields>/, /<\/fields>/ d ' "$file2/$j"
                     value=`sed -n '/<fields/,/<\/fields/p' "$file1/$j"`
 					echo "one"+$value >> newobj.object
                                 #sed -i ''"$location"'i ${value}' "$file2/$j"
