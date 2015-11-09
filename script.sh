80777d035047e990163f876baa00240ca024b48d
unknown <r.pallapotu@PDC1-87FCVQ1.dir.svc.accenture.com>
2015-11-09 18:23:55 +0530

    jh

--- script.sh
+++ script.sh
@@ -13,7 +13,8 @@ for j in `ls $file1`; do
                                 echo $location
                                 #echo "line number  of file2 $location"
                                 sed -i '/<fields>/, /<\/fields>/ d ' "$file2/$j"
-								value=`sed -n '/<fields/,/<\/fields/p' "$file1/$j"` >> newobj.object
+								value=`sed -n '/<fields/,/<\/fields/p' "$file1/$j"` > newobj.object
+								cat newobj.object
 								#echo $value >> newobj.object
 								sed -i ''$location'r newobj.object' "$file2/$j"
 								rm -rf newobj.object
